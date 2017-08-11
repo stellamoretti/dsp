f490c4b09cfa566f8cc9c617afcc0ba5b5eb4202
Stella Moretti <28527227+stellamoretti@users.noreply.github.com>
2017-08-11 22:36:25 +0930

    added file created in vim

---
+++ editors/vinegar.py
@@ -0,0 +1,2 @@
+print("To start writing in a VIM-created file you must first type the letter 'i'.")
+print("And then to go back to the commands you must hit 'ESC' to get out of insert mode, then type ':'.)
