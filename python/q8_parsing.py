dcc904e29961acf4c5c1d065f020c27d5246ac31
Stella Moretti <28527227+stellamoretti@users.noreply.github.com>
2017-08-16 17:08:23 +0930

    updated code for Q8

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -8,6 +8,8 @@
 import csv
 import pprint
 
+'''
+
 with open('football.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     dict_list = []
@@ -32,3 +34,55 @@ for t in dict_list:
 answer = min(differences, key=lambda x:x['Difference'])
 
 print answer['Team']
+
+'''
+
+# Alternative method based on HackerRank guidance:
+
+def read_data(filename):
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)
+        goals = list(reader)
+        for li in goals:
+            li[1:] = map(int, li[1:])
+        return goals
+
+goals = read_data('football.csv')
+
+def get_index_with_min_abs_score_difference(goals):
+    for team in goals:
+        diff = abs(team[5] - team[6])
+        team.append(diff)
+    min_diff = min([t[8] for t in goals])
+    count = 0
+    for team in goals:
+        if team[8] == min_diff:
+            count += 1
+    if count > 1:
+        return 'There is more than one match.'
+    else:
+        for team in goals:
+            if team[8] == min_diff:
+                index_value = goals.index(team)
+                return index_value
+
+index_value =  get_index_with_min_abs_score_difference(goals)
+
+def get_team(index_value, parsed_data):
+    match = parsed_data[index_value]
+    return match[0]
+
+print get_team(index_value, goals)
+
+
+
+
+
+
+
+
+
+
+
+#
